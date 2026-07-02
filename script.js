096b035cc52fd5dfd3ec511c57d186a66ce03762
ParfekutoNegy <jitekomojiketa@hotmail.com>
2026-07-03 00:28:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1547,13 +1547,29 @@ function generateQRCodeAsync() {
     return new Promise((resolve) => {
         const deckData = createDeckData();
         const json = JSON.stringify(deckData);
+        // デッキデータをURL用に変換
+        const encoded =
+        encodeURIComponent(
+            btoa(
+                unescape(
+                    encodeURIComponent(json)
+                )
+            )
+        );
+        // QRに入れる文字列
+        const qrText =
+        location.origin +
+        location.pathname +
+        "?deck=" +
+        encoded;
+
         const qrArea = document.getElementById("qr-preview");
         qrArea.innerHTML = "";
         const canvas = document.createElement("canvas");
         qrArea.appendChild(canvas);
         QRCode.toCanvas(
             canvas,
-            json,
+            qrText,
             { width: 400,
                 errorCorrectionLevel:"H",
                 margin:2
@@ -1688,7 +1704,7 @@ function loadDeckFromQR(json) {
     updateDeck();
     updateCandidateArea();
     //新規デッキとして保存
-    autoSaveDeck
+    autoSaveDeck()
     // 編集画面へ
     /*showBuilderScreen();*/
     alert("QRからデッキを読み込みました");
@@ -1773,3 +1789,29 @@ document.getElementById("qr-image-input")
     reader.readAsDataURL(file);
 });
 
+window.addEventListener("load", () => {
+    const params =
+        new URLSearchParams(location.search);
+    const deck =
+        params.get("deck");
+    if(!deck){
+        return;
+    }
+    try{
+        const json =
+        decodeURIComponent(
+            escape(
+                atob(deck)
+            )
+        );
+        loadDeckFromQR(json);
+        // URLを元に戻す
+        history.replaceState(
+            {},
+            "",
+            location.pathname
+        );
+    }catch(e){
+        alert("デッキデータを読み込めませんでした");
+    }
+});
